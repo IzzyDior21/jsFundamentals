044c3f350d2f54f29b849ec9934beee0989a8a36
Izzy Dior <shop218916@gmail.com>
2025-02-23 12:25:46 -0600

    02/06/25  Var's STRING, INTEGERS, and BOOLEANS Lesson 4 PROJECT AND CHALLENGE.

--- script.js
+++ script.js
@@ -8,27 +8,27 @@
 let name = "Izzy Dior"; // anything in quotation marks are strings
 const age = 35; //whole numbers are integers
 
-//console.log(name);
-//console.log(age);
+console.log(name);
+console.log(age);
 
-//console.log("Hello" + " " + name);
-//console.log("Hello " +  name);
+console.log("Hello" + " " + name);
+console.log("Hello " +  name);
 
-//console.log("Hello " + name + " Your age is " + age );
-//console.log(`Hello ${name}, you are ${age} years old.`);
+console.log("Hello " + name + " Your age is " + age );
+console.log(`Hello ${name}, you are ${age} years old.`);
 
 
-//let firstName = "Izzy";
-//let lastName = "Dior"
-//let fullName = firstName + " " + lastName;
-//conole.log(`Hello ${fullName} nice to meet you!!`);
+let firstName = "Izzy";
+let lastName = "Dior"
+let fullName = firstName + " " + lastName;
+conole.log(`Hello ${fullName} nice to meet you!!`);
 
 //npm i readline-sync, Installs prompt input for node (Terminal)
 const readline = require("readline-sync")
 
-//let userName = readline.question("What is your name? ");
-// console.log(`Hello ${userName}`);
+let userName = readline.question("What is your name? ");
+ console.log(`Hello ${userName}`)
 
-//let firstName = readline.question("What is your name? ");
-//let beyonce = readline.question("Are your ready for the Beyonce concert? ");
-//console.log(`Hell yeah!! ${firstName} I'm so excited to party with you!! `);
+let firstName2 = readline.question("What is your name? ");
+let beyonce = readline.question("Are your ready for the Beyonce concert? ");
+console.log(`Hell yeah!! ${firstName2} I'm so excited to party with you!! `);
